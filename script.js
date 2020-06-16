f73438acce985c5fc593307999fe792512036ada
victorsc45 <victorlitzau@gmail.com>
2020-06-16 14:42:49 -0400

    minified moment file script file and css file corrected and moment date correct in header

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-// A $( document ).ready() block.
 $(document).ready(function () {
-    $("#currentDay").textContent = (moment().format('ddd MMM Do, YYYY'));
+    let currentDateEl = moment().format("MMMM Do YYYY, h:mm:ss a");
+    // console.log("current", currentDateEl);
+    $("#currentDay").text(currentDateEl);
+    let hourEl = moment().format('LT');
+    console.log("hour", hourEl);
+
+
+
+
+
+
+
+
+
+
+
+
 });
-
-
-//targetcoloroftext = yourarray(math.floor(math.random() * yourarray.length));
-//$("$#yourid").css("color", tragetcoloroftext());
-
-    // Array holds all of the drinks available
-    // var hourList = [
-    //     9, 10, 11, 12, 1, 2, 3, 4, 5
-    // ];
-// possible sort array: yourarray.sort( function(){
-  //  Return 0.5-Math.random();
-//});
-    // This line of jQuery selects the div with the matching id (#drink-options)
-    // var hourDiv = $(".container");
-
-    // Here we loop through our array using the .each() method and append a new div with each iteration
-    // $.each(hourList, function (i, hour) {
-    //     hourDiv.append("<div>" + hour + "</div>");
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
