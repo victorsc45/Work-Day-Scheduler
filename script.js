30773e97daa411cca47c9bba55f2e97485b1c270
victorsc45 <victorlitzau@gmail.com>
2020-06-19 16:16:42 -0400

    documentation on js css and index

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
 
+    //use moment.js to create variable for current day and time
+
     let currentDateEl = moment().format("MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentDateEl);
+
+    //set variable to compare past, present and future textarea color
+
     let checkTime = moment().format("HH");
 
+    //array of objects to pull for the JSON calls and set to local storage per each hour
 
     let workHours = [{
         id: "0",
@@ -70,51 +76,70 @@ $(document).ready(function () {
     },
     ];
 
-    var tempWorkHours = localStorage.getItem("info")
+    //check to see if JSON object info is present in local storage 
+
+    let tempWorkHours = localStorage.getItem("info")
     if (tempWorkHours) {
         workHours = JSON.parse(localStorage.getItem("info"))
     }
 
+    // set the information in the textarea to local storage using JSON stringify of array
 
     function setInfo() {
-
         localStorage.setItem("info", JSON.stringify(workHours));
     }
 
+    //display the information saved in the textarea using a loop through array 
 
     function displayInfo() {
-
         $.each(workHours, function (i, element) {
             $("#" + element.id).val(element.info)
         })
     }
+
+    //parse out entire array and checkif there is information there else display it
+
     function init() {
         let storedInfo = JSON.parse(localStorage.getItem("hourly"));
         if (storedInfo) {
             hourly = storedInfo;
         }
-
         displayInfo();
     }
 
+    // loop through workhours function of i and referred to as currentHour
+
     $.each(workHours, function (i, currentHour) {
+
+        //set row element to form add class and append to DOM
+
         let rowEl = $("<form>");
         rowEl.addClass("row");
         $(".container").append(rowEl);
+
+        //set div hour element add class and show formatted text of current hour to DOM
 
         let hourEl = $("<div>");
         hourEl.addClass("col-2 hour");
         hourEl.text(currentHour.hour + ":00" + " " + currentHour.ante_post);
 
+        //set form element to div and add classes
+
         let formEl = $("<div>");
         formEl.addClass("col-8 form-group")
+
+        //set planner description area to div and and classes
 
         let plannerEl = $("<div>");
         plannerEl.addClass("col-8 description p-0");
 
+        //set textarea element to variable append to planner div
 
         let textAreaEl = $("<textarea>");
         plannerEl.append(textAreaEl);
+
+        //set textarea element attribute and check if current time is past present or future
+
         textAreaEl.attr("id", currentHour.id);
         if (currentHour.time < checkTime) {
             textAreaEl.addClass("past");
@@ -124,35 +149,42 @@ $(document).ready(function () {
             currentHour.time > checkTime;
             textAreaEl.addClass("future");
         }
+
+        //set save button element and add classes
+
         let saveBttn = $("<i></i>");
         saveBttn.addClass("far fa-save fa-lg");
         let savePlans = $("<button>");
         savePlans.addClass("col-2 saveBtn");
 
+        //append save button, hour element, planner element to row element
+
         savePlans.append(saveBttn);
         rowEl.append(hourEl, plannerEl, savePlans);
     })
 
+    // call initialization of JSON objects
+
     init();
 
-    // save just grab the ifno from the textare and save th info in the object into the array and then save the localsotrage
+    // click handler to grab the info from the textarea and save to info in the object into the array and then save to localsotrage
 
     $(".saveBtn").on("click", function (event) {
         event.preventDefault();
-        //console.log(this)
+
+        //get the value of text and id
+
         let text = $(this).siblings(".description").children().val()
         let ids = $(this).siblings(".description").children().attr("id")
-        //console.log(text, ids);
 
         // loop the local storage
+
         for (let i = 0; i < workHours.length; i++) {
             if (workHours[i].id === ids) {
                 workHours[i].info = text;
                 localStorage.setItem("info", JSON.stringify(workHours))
             }
-
         }
-
     })
 });
 
